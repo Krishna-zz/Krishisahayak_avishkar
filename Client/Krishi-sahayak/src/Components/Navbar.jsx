be95d8ade385a62c60ffdaa4984ecd83ad9900ce
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-26 00:26:24 +0530

    replace href with link

--- Client/Krishi-sahayak/src/Components/Navbar.jsx
+++ Client/Krishi-sahayak/src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
-
+import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
     <div className="w-full bg-green-700 text-white py-4 px-8 flex items-center justify-between shadow-md">
       <h1 className="text-2xl font-bold">KrishiSahayak</h1>
       <div className="flex gap-6">
-        <a href="/" className="hover:text-yellow-300">Home</a>
-        <a href="/dashboard" className="hover:text-yellow-300">Dashboard</a>
+       <Link to="/" className="hover:text-yellow-300">Home</Link>
+      <Link to="/dashboard" className="hover:text-yellow-300">Dashboard</Link>
       </div>
     </div>
   );
