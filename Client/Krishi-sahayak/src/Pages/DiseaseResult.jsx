4d503b4fad3d9fe52588e1380d271d64c12b0f81
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-26 00:07:37 +0530

    added disease result page

--- Client/Krishi-sahayak/src/Pages/DiseaseResult.jsx
+++ Client/Krishi-sahayak/src/Pages/DiseaseResult.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import Navbar from '../Components/Navbar';
 
 const DiseaseResult = () => {
+    const data = JSON.parse(localStorage.getItem("disease"));
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Navbar />
+
+      <div className="p-8 max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Disease Detection Result</h1>
+
+        {!data ? (
+          <p>No data found.</p>
+        ) : (
+          <div className="bg-white p-6 rounded-lg shadow-md space-y-4">
+            <h2 className="text-xl font-bold">Detected Disease:</h2>
+            <p>{data.label}</p>
+
+            <h2 className="text-xl font-bold">Confidence:</h2>
+            <p>{data.confidence}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default DiseaseResult
