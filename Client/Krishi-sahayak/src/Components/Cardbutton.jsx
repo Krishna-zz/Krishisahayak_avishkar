c6d53e2c8879f9b95f95c88b4ceffa59ab3d7b54
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-25 23:56:52 +0530

    Cardbutton issue fixed

--- Client/Krishi-sahayak/src/Components/Cardbutton.jsx
+++ Client/Krishi-sahayak/src/Components/Cardbutton.jsx
@@ -3,20 +3,19 @@ import { useNavigate } from 'react-router-dom';
 
 function Cardbutton({ title, desc, link }) {
 
-  const navigate = useNavigate()
+ const navigate = useNavigate();
 
-   return (
-    <a
-      href={link}
-      className="w-full md:w-1/3 p-6 bg-white rounded-xl shadow-lg border hover:scale-105 transition"
+  return (
+    <div
+      onClick={() => navigate(link)}
+      className="w-full md:w-1/3 p-6 bg-white rounded-xl shadow-lg border hover:scale-105 transition cursor-pointer"
     >
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-600 mb-4">{desc}</p>
-      <button className="bg-green-600 text-white px-4 py-2 rounded-lg"
-              onClick={() => navigate(link)}>
+      <button className="bg-green-600 text-white px-4 py-2 rounded-lg">
         Proceed →
       </button>
-    </a>
+    </div>
   );
 }
 
