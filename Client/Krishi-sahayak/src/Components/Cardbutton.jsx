5237c7283afc2e5c60dd37fc55a3c03c6282730a
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-25 13:46:10 +0530

    Made Cardbutton and Dashboard

---
+++ Client/Krishi-sahayak/src/Components/Cardbutton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Cardbutton({ title, desc, link }) {
+   return (
+    <a
+      href={link}
+      className="w-full md:w-1/3 p-6 bg-white rounded-xl shadow-lg border hover:scale-105 transition"
+    >
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p className="text-gray-600 mb-4">{desc}</p>
+      <button className="bg-green-600 text-white px-4 py-2 rounded-lg">
+        Proceed →
+      </button>
+    </a>
+  );
+}
+
+export default Cardbutton
