f66e4c73cafc1baa3f65457a17e9b1b77dc7ef3e
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-26 00:20:07 +0530

    Removed Cardwide onClick and put it only on the button

--- Client/Krishi-sahayak/src/Components/Cardbutton.jsx
+++ Client/Krishi-sahayak/src/Components/Cardbutton.jsx
@@ -6,16 +6,16 @@ function Cardbutton({ title, desc, link }) {
  const navigate = useNavigate();
 
   return (
-    <div
-      onClick={() => navigate(link)}
-      className="w-full md:w-1/3 p-6 bg-white rounded-xl shadow-lg border hover:scale-105 transition cursor-pointer"
-    >
-      <h2 className="text-xl font-bold mb-2">{title}</h2>
-      <p className="text-gray-600 mb-4">{desc}</p>
-      <button className="bg-green-600 text-white px-4 py-2 rounded-lg">
-        Proceed →
-      </button>
-    </div>
+   <div className="w-full md:w-1/3 p-6 bg-white rounded-xl shadow-lg border">
+  <h2 className="text-xl font-bold mb-2">{title}</h2>
+  <p className="text-gray-600 mb-4">{desc}</p>
+  <button
+    onClick={() => navigate(link)}
+    className="bg-green-600 text-white px-4 py-2 rounded-lg"
+  >
+    Proceed →
+  </button>
+</div>
   );
 }
 
